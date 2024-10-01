38f92716f5416119adb356460d3c5ded5df69da3
nguyensylinh <linhns3141@gmail.com>
2024-10-01 18:27:26 +0700

    init project

---
+++ Salary_Company_Project/Salary_Company_Project/TienthuongNgoaigio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class TienthuongNgoaigio : ITienthuong
+    {
+        public double tinhTienThuong(double luongCB)
+        {
+            // logic code ..............
+            return luongCB * 0.10;
+        }
+    }
+}
